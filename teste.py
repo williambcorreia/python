01ca8d3bf5f3d271e23f9096a7496f9db6c5d787
williambcorreia <wburgard6@gmail.com>
2025-08-20 11:30:37 -0300

    Initial commit

---
+++ teste.py
@@ -0,0 +1,10 @@
+comida = "Pizza"
+sabor = ""
+
+print ("Qual será a comida de hoje?")
+
+if comida == "pizza":
+    print ("O prato é: %f" % comida)
+else:
+    if comida == "pizza":
+        print ("Qual é o sabor?")
