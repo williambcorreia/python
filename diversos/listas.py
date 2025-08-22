e88aba3149b10d62c9f33f0208a037a77f82b688
williambcorreia <wburgard6@gmail.com>
2025-08-21 23:06:55 -0300

    código inserido

---
+++ diversos/listas.py
@@ -0,0 +1,31 @@
+# var
+L = [0, 1, 2, 3, 4]
+L2 = [5, 6, 7, 8]
+
+# código
+print (L)
+print () #linha em branco
+print (L2)
+print () #linha em branco
+print (L + L2)
+print () #linha em branco
+print (L2 * 5)
+print () #linha em branco
+print (2 in L)
+print (7 in L)
+print (7 in L2)
+print () #linha em branco
+for x in L:
+    print (x)
+L.append (9)
+L.append (11)
+L.append (2)
+L.append (2)
+L.insert (3, 12)
+print () #linha em branco
+print (L.index (12))
+print (L.index (9))
+print () #linha em branco
+print (L)
+print () #linha em branco
+print (L.count (2))
